ddcfedafad1c423868445c9644f5d2a48c2d1a0d
KhalgaevDavid <davidxalgaev@gmail.com>
2026-06-19 16:31:53 +0300

    Базовая часть

---
+++ scales.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var left_f: float
+var right_f: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func round_to_dec(num, digit) -> float:
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
+
+func update_label(mass: float) -> void:
+	var curr_mass: float = float($Label.text)
+	curr_mass += mass
+	curr_mass = round_to_dec(curr_mass, 3)
+	$Label.text = str(curr_mass)
+
+func left_body_entered(body: RigidBody2D) -> void:
+	var mass = body.get_mass()
+	update_label(mass)
+
+
+func left_body_exited(body: RigidBody2D) -> void:
+	var mass = -body.get_mass()
+	update_label(mass)
+
+
+func right_body_entered(body: RigidBody2D) -> void:
+	var mass = body.get_mass()
+	update_label(mass)
+
+
+func right_body_excited(body: RigidBody2D) -> void:
+	var mass = -body.get_mass()
+	update_label(mass)
