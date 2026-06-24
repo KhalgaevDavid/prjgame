c1d829857df4b495eb2d14880a694954ab7dae7f
KhalgaevDavid <davidxalgaev@gmail.com>
2026-06-24 19:05:13 +0300

    стрелка панели и панель

--- scales.gd
+++ scales.gd
@@ -3,6 +3,9 @@ extends Node2D
 var left_f: float
 var right_f: float
 
+@onready var left_label = $Left/Mass
+@onready var right_label = $Right/Mass
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,27 +18,27 @@ func _process(delta: float) -> void:
 func round_to_dec(num, digit) -> float:
 	return round(num * pow(10.0, digit)) / pow(10.0, digit)
 
-func update_label(mass: float) -> void:
-	var curr_mass: float = float($Label.text)
+func update_label(mass: float, label: Label) -> void:
+	var curr_mass: float = float(label.text)
 	curr_mass += mass
 	curr_mass = round_to_dec(curr_mass, 3)
-	$Label.text = str(curr_mass)
+	label.text = str(curr_mass)
 
 func left_body_entered(body: RigidBody2D) -> void:
 	var mass = body.get_mass()
-	update_label(mass)
+	update_label(mass, left_label)
 
 
 func left_body_exited(body: RigidBody2D) -> void:
 	var mass = -body.get_mass()
-	update_label(mass)
+	update_label(mass, left_label)
 
 
 func right_body_entered(body: RigidBody2D) -> void:
 	var mass = body.get_mass()
-	update_label(mass)
+	update_label(mass, right_label)
 
 
 func right_body_excited(body: RigidBody2D) -> void:
 	var mass = -body.get_mass()
-	update_label(mass)
+	update_label(mass, right_label)
